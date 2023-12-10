a5fb72f66a1c53d6a4337f58206fc6ce13a470ee
Yidnekachew Tebeje <yidnekachewtebeje@gmail.com>
2023-12-10 03:14:29 +0300

    Remove unnecessary code comment

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 import { ContestStanding, addUser, getRatings } from "../codeforcequery";
-export const dynamic = 'force-dynamic' // defaults to force-static
+ // defaults to force-static
 export async function GET(req: NextRequest){
     const data = await getRatings()
     return Response.json(data)
